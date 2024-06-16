1986ce703226dedd7c306c51a320c64680d702e0
KalWantsPizza <52138518+KalWantsPizza@users.noreply.github.com>
2024-06-16 16:53:38 -0400

    missed one

--- common/src/main/java/example/modname/Pizzeria.java
+++ common/src/main/java/example/modname/Pizzeria.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class pizzeria {
-    public static final String MOD_ID = "vinery";
+    public static final String MOD_ID = "pizzeria";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     
     public static void init() {
